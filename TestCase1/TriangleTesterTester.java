01a1fb7a38da8861693b3b94edb56382fcf3fdd7
Robert Campbell <robertgcampbell95@gmail.com>
2019-02-09 13:18:38 -0800

    Add junit tester with test case stubs

---
+++ TestCase1/TriangleTesterTester.java
@@ -0,0 +1,21 @@
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TriTestTester {
+
+	@Test
+	public void testValidSide() {
+    }
+	
+	@Test
+	public void testValidTriangle() {
+    }
+	
+	@Test
+	public void testTypeOf() {
+    }
+    
+ }
